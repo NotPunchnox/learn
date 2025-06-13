bc0b9ba77ea1ecbc8c95a9ede4c3cdf836bc2108
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-06-13 21:33:50 +0200

    Improve loss function stability and adjust training parameters; enhance backpropagation logic in MLP

--- python/mlp/main.py
+++ python/mlp/main.py
@@ -4,6 +4,8 @@ from math import *
 
 # Fonction de calcul du taux d'erreur ( log-vraisemblance négative )
 def Loss(prediction, sortie_attendue):
+    epsilon = 1e-10
+    prediction = max(min(prediction, 1 - epsilon), epsilon)
     return -(sortie_attendue * np.log(prediction) + (1 - sortie_attendue) * np.log(1 - prediction))
 
 # Fonction sigmoïde
@@ -19,17 +21,17 @@ def sigmoide_derivative(y):
 def main():
 
     # constantes
-    learning_rate = 0.1
-    max_epochs = 5000
+    learning_rate = 0.5
+    max_epochs = 10000
     nb_layers = 3
-    nb_neurons_per_layers = [3, 3, 1]
+    nb_neurons_per_layers = [3, 3, 1, 8]
     nb_inputs = 2
 
     # Données d'entrainement ( XOR )
     x = [
         [0, 0],
-        [1, 0],
         [0, 1],
+        [1, 0],
         [1, 1],
     ]
     yy = [0, 1, 1, 0]
@@ -57,7 +59,6 @@ def main():
                 for input in range(nb_neurons_per_layers[layer -1]):
                     w[layer][neuron].append(random.random())
 
-
     print(b)
     print(w)
 
@@ -84,59 +85,64 @@ def main():
                         for k in range(nb_inputs):
                             layer_inputs[layer][n] += x[i][k] * w[layer][n][k]
                     else:
-                        for k in range(len(w[layer][n])):
+                        for k in range(nb_neurons_per_layers[layer-1]):
                             layer_inputs[layer][n] += layer_output[layer-1][k] * w[layer][n][k]
 
                     layer_output[layer][n] = sigmoide(layer_inputs[layer][n])
             
-            final_output = layer_output[nb_layers-1][nb_neurons_per_layers[nb_layers-1]-1]
+            final_output = layer_output[nb_layers-1][0]
             
             # Calcul du taux d'erreur
             total_error += Loss(final_output, yy[i])
 
             # Rétropropagation
-            delta = (final_output - yy[i]) * sigmoide_derivative(final_output)
-            
-            for layer in reversed(range(nb_layers)):
-                for n in range(nb_neurons_per_layers[layer]):
-
-                    if layer == nb_layers-1:
-                        for k in range(len(w[layer][n])):
-                            w[layer][n][k] -= learning_rate * delta * layer_output[layer][n]
-                        b[layer][n] -= learning_rate * delta
-                    else:
-                        delta_hidden = 0
-                        for k in range(len(w[layer][n])):
-                            delta_hidden += delta * w[layer][n][k] * sigmoide_derivative(layer_inputs[layer][n])
-                            w[layer][n][k] -= learning_rate * delta_hidden * layer_output[layer][k]
-                        b[layer][n] -= learning_rate * delta_hidden
-
-        # Afficher les stats
-        print(f"Epoch: {epoch}, Erreur moyenne: {total_error}")
-
-        for i in range(len(x)):
-            # Lister les couches
+            deltas = {}
             for layer in range(nb_layers):
-                layer_inputs[layer] = {}
-                layer_output[layer] = {}
-
-                # Lister les neurones de la couche ( layer )
+                deltas[layer] = {}
                 for n in range(nb_neurons_per_layers[layer]):
-                    layer_inputs[layer][n] = b[layer][neuron]
-                    
+                    deltas[layer][n] = 0
+
+            deltas[nb_layers-1][0] = (final_output - yy[i]) * sigmoide_derivative(layer_inputs[nb_layers-1][0])
+
+            for layer in reversed(range(nb_layers-1)):
+                for n in range(nb_neurons_per_layers[layer]):
+                    for next_n in range(nb_neurons_per_layers[layer+1]):
+                        deltas[layer][n] += deltas[layer+1][next_n] * w[layer+1][next_n][n]
+                    deltas[layer][n] *= sigmoide_derivative(layer_inputs[layer][n])
+
+            for layer in range(nb_layers):
+                for n in range(nb_neurons_per_layers[layer]):
                     if layer == 0:
                         for k in range(nb_inputs):
-                            layer_inputs[layer][n] += x[i][k] * w[layer][n][k]
+                            w[layer][n][k] -= learning_rate * deltas[layer][n] * x[i][k]
                     else:
-                        for k in range(len(w[layer][n])):
-                            layer_inputs[layer][n] += layer_output[layer-1][n]
+                        for k in range(nb_neurons_per_layers[layer-1]):
+                            w[layer][n][k] -= learning_rate * deltas[layer][n] * layer_output[layer-1][k]
+                    b[layer][n] -= learning_rate * deltas[layer][n]
 
-                    layer_output[layer][n] = sigmoide(layer_inputs[layer][n])
-            
-            final_output = layer_output[nb_layers-1][nb_neurons_per_layers[nb_layers-1]-1]
-            print(f"Entrée: ({x[i][0]}, {x[i][1]}), Sortie: {final_output}, Attendu: {yy[i]}")
+        # Afficher les stats
+        print(f"Epoch: {epoch}, Erreur moyenne: {total_error / len(x)}")
+
+    for i in range(len(x)):
+        # Lister les couches
+        for layer in range(nb_layers):
+            layer_inputs[layer] = {}
+            layer_output[layer] = {}
+
+            # Lister les neurones de la couche ( layer )
+            for n in range(nb_neurons_per_layers[layer]):
+                layer_inputs[layer][n] = b[layer][n]
+                
+                if layer == 0:
+                    for k in range(nb_inputs):
+                        layer_inputs[layer][n] += x[i][k] * w[layer][n][k]
+                else:
+                    for k in range(nb_neurons_per_layers[layer-1]):
+                        layer_inputs[layer][n] += layer_output[layer-1][k] * w[layer][n][k]
+
+                layer_output[layer][n] = sigmoide(layer_inputs[layer][n])
+        
+        final_output = layer_output[nb_layers-1][0]
+        print(f"Entrée: ({x[i][0]}, {x[i][1]}), Sortie: {final_output}, Attendu: {yy[i]}")
 
 main()
-
-
-# By NotPunchnox
