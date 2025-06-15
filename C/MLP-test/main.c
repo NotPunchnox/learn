c6e2701961a3605624cfd42234fd3a1321cd7b2e
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-06-15 19:17:35 +0200

    Refactor memory allocation for weights and biases in MLP and add print functionality for debugging

--- C/MLP-test/main.c
+++ C/MLP-test/main.c
@@ -39,23 +39,41 @@ int main() {
   };
   const int yy[4] = {0, 1, 1, 0};
 
+  // Définir les poids et biais dans la mémoire afin d'avoir des tableaux dynamiques
+    double **b = (double **)malloc(nb_layers * sizeof(double));
+    double ***w = (double ***)malloc(nb_layers * sizeof(double));
+
   // Lister les couches
   for (int layer = 0; layer < nb_layers; layer++) {
 
-    // Définir les poids et biais dans la mémoire afin d'avoir des tableaux dynamiques
-    double **b = (double **)malloc(nb_layers * sizeof(double));
-    double **w = (double **)malloc(nb_layers * sizeof(double));
- 
+    // Lister les neurones présent dans la couche et leurs attitrer un biais
     for (int neuron = 0; neuron < nb_neurons_per_layer[layer]; neuron ++) {
-      *b = (double *)malloc(nb_neurons_per_layer[layer] *sizeof(double));
+      *b = (double *)malloc(nb_neurons_per_layer[layer] * sizeof(double));
+      **w = *(double **)malloc(nb_neurons_per_layer[layer] * sizeof(double));
+
       b[layer][neuron] = ((double)rand() / RAND_MAX) * 0.2 - 0.1;
 
-      for (int input = 0; input < nb_inputs; input ++) {
-
+      // Lister le nombre d'entrées
+      for (int input = 0; input < (layer == 0 ? nb_inputs : nb_neurons_per_layer[layer -1]); input ++) {
+        w[layer][neuron][input] = (double)rand() / RAND_MAX;
       }
 
     }
  
+  };
+  
+  // Print weights (w) and biases (b)
+  for (int layer = 0; layer < nb_layers; layer++) {
+    printf("Layer %d:\n", layer);
+    for (int neuron = 0; neuron < nb_neurons_per_layer[layer]; neuron++) {
+      printf("  Neuron %d:\n", neuron);
+      printf("    Bias: %f\n", b[layer][neuron]);
+      printf("    Weights: ");
+      for (int input = 0; input < (layer == 0 ? nb_inputs : nb_neurons_per_layer[layer - 1]); input++) {
+        printf("%f ", w[layer][neuron][input]);
+      }
+      printf("\n");
+    }
   }
 
   /*
