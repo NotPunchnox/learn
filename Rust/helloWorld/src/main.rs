f10672f2b2cbe124db928334ef869bf822fc6c67
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-18 16:04:18 +0200

    Ajout de fichiers de configuration Rust et mise à jour du fichier .gitignore pour inclure les cibles et les fichiers de verrouillage

---
+++ Rust/helloWorld/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    // test de println simple
+    let name = "punchnox";
+    println!("Hello, world!\n");
+    println!("Nom: {}", name);
+
+
+    // Test de variable (let x = constante)
+    let mut test = "salut";
+    println!("Variable test: {}", test);
+    
+    test = "salut ça va ?";
+    println!("Variable test après modification: {}", test);
+
+}
