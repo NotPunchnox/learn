14d9064ae3c611d2c8645836ddc580c5dfa0c1d3
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-27 15:28:33 +0100

    Correction du type d'objet de Oiseau à Reptile dans main.cpp et ajout d'une nouvelle ligne dans Oiseau.cpp pour respecter le formatage.

--- C++/Zoo/main.cpp
+++ C++/Zoo/main.cpp
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-    Oiseau lion(1, "Simba", "Panthera leo", 5, 190.5, 101, "dorée");
+    Reptile lion(1, "Simba", "Panthera leo", 5, 190.5, 101, "dorée");
     
     lion.afficherInfos();
     lion.emettreSon();
