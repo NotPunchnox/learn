89aeccafd1f7230a10666bffcc9d15e89a2fb4e6
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-28 15:29:56 +0100

    Réorganisation de main.cpp : suppression des tests de classes spécifiques et intégration de la récupération des animaux depuis la base de données via AnimalDao.

--- C++/Zoo/main.cpp
+++ C++/Zoo/main.cpp
@@ -1,41 +1,20 @@
-#include "include/models/Mammifere.hpp"
-#include "include/models/Oiseau.hpp"
-#include "include/models/Reptile.hpp"
-#include "include/models/Enclos.hpp"
-#include "include/models/Employe.hpp"
+#include <iostream>
+#include "include/dao/AnimalDao.hpp"
 
-int test(void) {
-     // Test de la classe Mammifere
-    Mammifere lion(1, "Simba", "Panthera leo", 5, 190.5, 101, "dorée");
-    
-    lion.afficherInfos();
-    lion.emettreSon();
-
-    // Test de la classe Oiseau
-    Oiseau perroquet(2, "Polly", "Amazona aestiva", 2, 1.5, 102, "30 cm");
-
-    // Test de la classe Reptile
-    Reptile serpent(3, "Kaa", "Python regius", 4, 12.3, 103, true);
-
-
-    // Test de la classe Enclos
-    Enclos petitEnclo(1, "petit", 20.5, 2);
-    petitEnclo.ajouterAnimal(serpent);
-    petitEnclo.ajouterAnimal(perroquet);
-
-    petitEnclo.afficherAnimaux();
-    petitEnclo.getNbAnimaux();
-
-
-    // Test de la classe Employe
-    Employe unEmploye(1, "Jhon", "Garde", 1.5, petitEnclo.getID());
-    unEmploye.afficherInfos();
-
-
-    return 0;
-}
 
 int main(void)
 {
-   test();
+
+   DatabaseManager *db = new DatabaseManager("localhost", "root", "password", "zoo_db");
+
+   AnimalDao animalDao(db);
+   std::vector<Animal> listeAnimaux = animalDao.getAll();
+
+   std::cout << "Nombre d'animaux dans le Zoo : " << listeAnimaux.size() << std::endl;
+
+   for (Animal animal : listeAnimaux)
+   {
+      animal.afficherInfos();
+   }
+   
 }
