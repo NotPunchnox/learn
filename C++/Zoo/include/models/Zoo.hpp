6d1a4512c873fbf3897ff269fcb563da9259a84e
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2025-10-28 13:10:36 +0100

    Ajout de la classe Zoo avec ses constructeurs, getters, setters et méthodes pour gérer les enclos et les employés.

---
+++ C++/Zoo/include/models/Zoo.hpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include "./Employe.hpp"
+#include "./Enclos.hpp"
+
+
+class Zoo {
+
+public:
+
+    // Constructeur
+    Zoo();
+    Zoo(const std::vector<Enclos> enclosList, const std::vector<Employe> employeList);
+
+    // Getters
+    const std::vector<Enclos> getEnclos() const;
+    const std::vector<Employe> getEmploye() const;
+
+    // Setters
+    void setEnclos(const std::vector<Enclos> enclosList);
+    void setEmploye(const std::vector<Employe> employeList);
+
+
+    // Méthodes
+    void chargerDepuisDB();
+    void sauvegarderDansDB();
+    void simulerJournee();
+    void afficherZoo() const;
+
+private:
+    std::vector<Enclos> enclosList;
+    std::vector<Employe> employeList;
+
+};
